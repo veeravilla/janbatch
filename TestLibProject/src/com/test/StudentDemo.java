36a57c30e89456498cf29fe0ae29168b96519a09
Veeru <Veeru@Veeru-PC>
2016-01-20 18:35:54 -0600

    Lib Demo

---
+++ TestLibProject/src/com/test/StudentDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.test.util.MapExample3;
+import com.test.util.Student;
+
+public class StudentDemo {
+
+	public static void main(String[] args)  throws  Exception{
+		Student s = new Student(101, "Raama", 1234);
+		MapExample3.main(args);
+
+	}
+
+}
